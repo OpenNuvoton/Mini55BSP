6e1ede520eeaa88097238d337c983c7df858736c
Yi-An Chen <yachen@nuvoton.com>
2017-04-24 15:53:37 +0800

    Update for 3.01.001 release
    Change-Id: If5d0ed6525cd99c50843575ae878acf35cc476a6

---
+++ SampleCode/NuTiny-SDK-MINI55/LED_Toggle/main.c
@@ -0,0 +1,97 @@
+/**************************************************************************//**
+ * @file     main.c
+ * @version  V2.10
+ * $Date: 15/07/09 9:41a $
+ * @brief    Toggle P1.5 to turn on/off LED.
+ *
+ * @note
+ * Copyright (C) 2014 Nuvoton Technology Corp. All rights reserved.
+ *
+ ******************************************************************************/
+#include <stdio.h>
+#include "Mini55series.h"
+#include "GPIO.h"
+
+void SYS_Init(void)
+{
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Read User Config to select internal high speed RC */
+    SystemInit();
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init System Clock                                                                                       */
+    /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Enable external 12MHz XTAL, HIRC */
+    CLK->PWRCTL |= CLK_PWRCTL_XTL12M | CLK_PWRCTL_HIRCEN_Msk;
+
+    /* Waiting for clock ready */
+    CLK_WaitClockReady(CLK_STATUS_XTLSTB_Msk | CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to XTL */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_XTAL,CLK_CLKDIV_HCLK(1));
+
+    /* STCLK to XTL STCLK to XTL */
+    CLK_SetSysTickClockSrc(CLK_CLKSEL0_STCLKSEL_XTAL);
+
+    /* Enable IP clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select IP clock source */
+    CLK_SetModuleClock(UART0_MODULE,CLK_CLKSEL1_UART0SEL_XTAL,CLK_CLKDIV_UART(1));
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Set P0 multi-function pins for UART RXD and TXD */
+    SYS->P0_MFP &= ~(SYS_MFP_P01_Msk | SYS_MFP_P00_Msk);
+    SYS->P0_MFP |= (SYS_MFP_P01_RXD | SYS_MFP_P00_TXD);
+
+    /* To update the variable SystemCoreClock */
+    SystemCoreClockUpdate();
+
+    /* Lock protected registers */
+    SYS_LockReg();
+}
+
+void UART_Init(void)
+{
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init UART                                                                                               */
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset IP */
+    SYS_ResetModule(SYS_IPRST1_UART0RST_Msk);
+
+    /* Configure UART and set UART Baudrate */
+    UART_Open(UART0, 115200);
+
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/* MAIN function                                                                                           */
+/*---------------------------------------------------------------------------------------------------------*/
+int main (void)
+{
+    /* Init System, IP clock and multi-function I/O */
+    SYS_Init(); //In the end of SYS_Init() will issue SYS_LockReg() to lock protected register. If user want to write protected register, please issue SYS_UnlockReg() to unlock protected register.
+
+    /* Init UART for printf */
+    UART_Init();
+
+    printf("\n\nCPU @ %dHz\n", SystemCoreClock);
+
+    printf("+-------------------------------------+ \n");
+    printf("|    MINI55 Toggle LED Sample Code    | \n");
+    printf("+-------------------------------------+ \n");
+
+    /* Configure P1.5 as Output mode */
+    P15 = 1;
+    GPIO_SetMode(P1, BIT5, GPIO_MODE_OUTPUT);
+
+    while (1) {
+        P15 ^= 1;
+        CLK_SysTickDelay(250000);
+    }
+}
