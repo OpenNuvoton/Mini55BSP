6f8f5d25633d16c59c09d8e2316bdbe8038af0d3
cwweng <cwweng@nuvoton.com>
2019-11-01 16:18:20 +0800

    Add ISP SPI sample code
    Change-Id: I0badc530c048590db3ab26ed4d1a23a3d1a67358

---
+++ SampleCode/ISP/ISP_SPI/targetdev.h
@@ -0,0 +1,18 @@
+/***************************************************************************//**
+ * @file     targetdev.h
+ * @brief    ISP support function header file
+ * @version  0x32
+ * @date     14, June, 2017
+ *
+ * @note
+ * Copyright (C) 2017-2018 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#ifndef __TARGET_H__
+#define __TARGET_H__
+
+#include "Mini55Series.h"
+#include "ISP_USER.h"
+
+#define I2C_ADDR 0x60
+
+#endif //__TARGET_H__
